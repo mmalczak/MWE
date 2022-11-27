5e7b2d61f7d2b71980c7f837e6c6cae77a1774f7
Adrian Kurylak <adrian.kurylak@gmail.com>
2022-11-27 18:17:41 +0100

    Draft

--- tests/basic_test.cc
+++ tests/basic_test.cc
@@ -1,6 +1,32 @@
+#include "common/include/matrix.hpp"
+
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
+constexpr int32_t max_rows = 32;
+constexpr int32_t max_cols = 32;
+
+class BasicTest : public ::testing::Test {
+public:
+  float m1_data[max_rows][max_cols]{};
+  float m2_data[max_rows][max_cols]{};
+  float exp_out[max_rows][max_cols]{};
+};
+
+TEST_F(BasicTest, test_2x2) {
+  constexpr int32_t rows = 2;
+  constexpr int32_t columns = 2;
+
+  matrix::Matrix m1{.n_rows = rows, .n_columns = columns, .data = &m1_data[0][0]};
+  matrix::Matrix m2{.n_rows = rows, .n_columns = columns, .data = &m2_data[0][0]};
+
+  float out_data[max_rows][max_cols]{};
+  matrix::Matrix out{.n_rows = rows, .n_columns = columns, .data = &out_data[0][0]};
+
+  matrix::multiply(m1.n_rows, m1.n_columns, m2.n_columns, m1.data, m2.data, out.data);
+
+  for (int32_t i = 0; i < rows; i++) {
+    for (int32_t k = 0; k < columns; k++) {
+      EXPECT_NEAR(out.data[i * columns + k], exp_out[i][k], 1e-4f);
+    }
+  }
 }
