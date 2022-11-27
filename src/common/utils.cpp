92557a5db2dd65ee03dd5099a776c3b29e8301e0
Milosz Malczak <milosz.malczak@gmail.com>
2022-11-27 16:51:08 +0100

    remove redundant idx

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -10,8 +10,7 @@ namespace utils {
 void print(int M, int N, float *data) {
   for (int j = 0; j < M; j++) {
     for (int i = 0; i < N; i++) {
-      int idx = i + j * N;
-      printf("%f ", data[idx]);
+      printf("%f ", *data++);
     }
     printf("\n");
   }
