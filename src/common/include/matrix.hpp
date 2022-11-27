c609adb3e7f9693bff4a787d681eaeefcc6499a3
Milosz Malczak <milosz.malczak@gmail.com>
2022-11-27 17:52:58 +0100

    Fixed header

--- src/common/include/matrix.hpp
+++ src/common/include/matrix.hpp
@@ -7,6 +7,6 @@ struct Matrix {
   float *data;
 };
 
-Matrix *multiply(int32_t M, int32_t P, int32_t N, float *A, float *B, float *C);
+void multiply(int32_t M, int32_t P, int32_t N, float *A, float *B, float *C);
 
 } // namespace matrix
