aec740ad470d7517050148ede824e211aabe2dc0
Adrian Wysocki <adrian.kurylak@gmail.com>
2022-11-27 13:49:31 +0100

    Refactore CMake files, Cleaned-up code, added formatting scripts and some minor cosmetics

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # MWE
+
+## Code formatting
+> ./scripts/code_format.sh
+
+## Build
+
+### Debug
+> cmake -B ${PWD}/build/ -D CMAKE_BUILD_TYPE="Debug" -G Ninja
+> ninja -C ./build all
+
+### Release
+> cmake -B ${PWD}/build/ -D CMAKE_BUILD_TYPE="Release" -G Ninja
+> ninja -C ./build all
+
+## Run app
+> ./build/apps/mwe
+
+## Run Unit Tests
+> cd build && ctest --verbose
--- apps/mwe.cpp
+++ apps/mwe.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-#include <mwe/matrix.hpp>
+#include "matrix.hpp"
 
-int main()
-{
-    matrix::Matrix* m = matrix::generateMatrix(2, 2);
-    matrix::printMatrix(2, 2, m->data);
-    float data[] = {1, 2, 3, 4};
-    matrix::printMatrix(2, 2, data);
-    return 0;
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  matrix::Matrix *m = matrix::generateMatrix(2, 2);
+  matrix::printMatrix(2, 2, m->data);
+  float data[] = {1, 2, 3, 4};
+  matrix::printMatrix(2, 2, data);
 }
