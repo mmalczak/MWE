f663534850627dae5dc4803410b338bb8ce86471
Milosz Malczak <milosz.malczak@gmail.com>
2022-11-27 21:18:19 +0100

    Add basic example

--- examples/mwe.cpp
+++ examples/mwe.cpp
@@ -2,6 +2,12 @@
 #include "common/include/utils.hpp"
 
 int main(int argc, char *argv[]) {
-  float data[] = {1, 2, 3, 4};
-  utils::print(2, 2, data);
+  float A[] = {1, 2, 3, 4};
+  float B[] = {1, 2, 3, 4};
+  float C[] = {0, 0, 0, 0};
+  int32_t M = 2;
+  int32_t P = 2;
+  int32_t N = 2;
+  matrix::multiply(M, P, N, A, B, C);
+  utils::print(M, N, C);
 }
