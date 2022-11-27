a1ce1fe80e7534767f892f706646593eb828a563
Adrian Wysocki <adrian.kurylak@gmail.com>
2022-11-27 17:02:04 +0100

    Added utils to load data from binary files

--- src/common/include/utils.hpp
+++ src/common/include/utils.hpp
@@ -1,5 +1,10 @@
+#include <string>
+#include <vector>
+
 namespace utils {
 
 void print(int M, int N, float *data);
+
+template <typename T> void read_blob_file(const std::string &filename, std::vector<T> &out);
 
 } // namespace utils
