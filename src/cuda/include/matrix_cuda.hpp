f216b5103a590e741a19abcb37b5df8f52370503
Milosz Malczak <milosz.malczak@gmail.com>
2022-12-19 09:11:41 +0100

    Remove cuda samples and add basic cuda matmul to sources

---
+++ src/cuda/include/matrix_cuda.hpp
@@ -0,0 +1,5 @@
+
+namespace matrix_cuda {
+void multiply_matrix(float *C, float *A, float *B, int M, int P, int N);
+
+} // namespace matrix_cuda
---
+++ src/cuda/matrix_cuda.cu
@@ -0,0 +1,43 @@
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <helper_cuda.h>
+
+namespace matrix_cuda {
+
+__global__ static void matmul_kernel(float *C, float *A, float *B, int m, int p, int n) {
+  int i = threadIdx.y;
+  int j = threadIdx.x;
+  int k;
+  float s = 0;
+  for (k = 0; k < p; k++) {
+    s += A[i * p + k] * B[k * n + j];
+  }
+  C[i * n + j] = s;
+}
+
+void multiply_matrix(float *C, float *A, float *B, int M, int P, int N) {
+  float *devA, *devB, *devC;
+  checkCudaErrors(cudaSetDevice(0));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devA), M * P * sizeof(float)));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devB), P * N * sizeof(float)));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devC), M * N * sizeof(float)));
+
+  checkCudaErrors(cudaMemcpy(devA, A, M * P * sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(devB, B, P * N * sizeof(float), cudaMemcpyHostToDevice));
+
+  dim3 dimBlock(N, M);
+
+  matrix_cuda::matmul_kernel<<<1, dimBlock>>>(devC, devA, devB, M, P, N);
+
+  checkCudaErrors(cudaGetLastError());
+
+  checkCudaErrors(cudaDeviceSynchronize());
+
+  checkCudaErrors(cudaMemcpy(C, devC, M * N * sizeof(float), cudaMemcpyDeviceToHost));
+
+  checkCudaErrors(cudaFree(devA));
+  checkCudaErrors(cudaFree(devB));
+  checkCudaErrors(cudaFree(devC));
+}
+
+} // namespace matrix_cuda
