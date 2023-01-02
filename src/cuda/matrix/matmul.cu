3a7ae92b7c179997441f690c12ee9473fd3b10d5
mmalczak <milosz.malczak@gmail.com>
2023-01-02 22:44:55 +0100

    Matmul implementation was restricted by the block size. Calculate index based on both threadIdx and blockIdx (#23)

--- src/cuda/matrix/matmul.cu
+++ src/cuda/matrix/matmul.cu
@@ -5,35 +5,41 @@
 namespace matrix {
 
 __global__ static void multiply_kernel(float *C, float *A, float *B, int m, int p, int n) {
-  int i = threadIdx.y;
-  int j = threadIdx.x;
-  int k;
-  float s = 0;
-  for (k = 0; k < p; k++) {
-    s += A[i * p + k] * B[k * n + j];
+  int i = blockIdx.y * blockDim.y + threadIdx.y;
+  if(i<m) {
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    if(j<n) {
+      int k;
+      float s = 0;
+      for (k = 0; k < p; k++) {
+        s += A[i * p + k] * B[k * n + j];
+      }
+      C[i * n + j] = s;
+    }
   }
-  C[i * n + j] = s;
 }
 
-void multiply(float *C, float *A, float *B, int M, int P, int N) {
+void multiply(float *C, float *A, float *B, int m, int p, int n) {
   float *devA, *devB, *devC;
+  int N = 32;
+  int M = 32;
   checkCudaErrors(cudaSetDevice(0));
-  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devA), M * P * sizeof(float)));
-  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devB), P * N * sizeof(float)));
-  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devC), M * N * sizeof(float)));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devA), m * p * sizeof(float)));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devB), p * n * sizeof(float)));
+  checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&devC), m * n * sizeof(float)));
 
-  checkCudaErrors(cudaMemcpy(devA, A, M * P * sizeof(float), cudaMemcpyHostToDevice));
-  checkCudaErrors(cudaMemcpy(devB, B, P * N * sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(devA, A, m * p * sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(devB, B, p * n * sizeof(float), cudaMemcpyHostToDevice));
 
   dim3 dimBlock(N, M);
-
-  matrix::multiply_kernel<<<1, dimBlock>>>(devC, devA, devB, M, P, N);
+  dim3 dimGrid((n+N-1)/N, (m+M-1)/M);
+  matrix::multiply_kernel<<<dimGrid, dimBlock>>>(devC, devA, devB, m, p, n);
 
   checkCudaErrors(cudaGetLastError());
 
   checkCudaErrors(cudaDeviceSynchronize());
 
-  checkCudaErrors(cudaMemcpy(C, devC, M * N * sizeof(float), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(C, devC, m * n * sizeof(float), cudaMemcpyDeviceToHost));
 
   checkCudaErrors(cudaFree(devA));
   checkCudaErrors(cudaFree(devB));
