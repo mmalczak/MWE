5d440be1751c0bd8827d752d40403649464ea36b
Miłosz Malczak <milosz.malczak@gmail.com>
2023-01-12 01:02:07 +0100

    Add benchmarks of multiplication and multiplication kernel in ranges. Remove benchmark which read data from a blob file

--- tests/benchmarks/basic_benchmark.cc
+++ tests/benchmarks/basic_benchmark.cc
@@ -1,37 +1,41 @@
 #include "src/cuda/matrix/include/matrix.hpp"
 #include "src/cuda/matrix/include/matmul.hpp"
 #include "tests/common/include/matrix_test.hpp"
-
 #include <cstdlib>
 
 #include <benchmark/benchmark.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
 
 static void BM_BasicSquare(benchmark::State &state) {
-  float A[4] = {1, 2, 3, 4};
-  float B[4] = {1, 2, 3, 4};
-  int32_t M = 2;
-  int32_t P = 2;
-  int32_t N = 2;
-  float res[4] = {0, 0, 0, 0};
+  void *A, *B, *C;
+  int32_t m = state.range(0);
+  int32_t p = state.range(0);
+  int32_t n = state.range(0);
+  A = malloc(m * p * sizeof(float));
+  B = malloc(p * n * sizeof(float));
+  C = malloc(m * n * sizeof(float));
   for (auto _ : state)
-    matrix::multiply(res, A, B, M, P, N);
+    matrix::multiply(static_cast<float *>(C), static_cast<float *>(A), static_cast<float *>(B), m, p, n);
 }
 
-BENCHMARK(BM_BasicSquare);
+BENCHMARK(BM_BasicSquare)->RangeMultiplier(2)->Range(2, 1 << 10);
 
-static void BM_WithBinaryFiles(benchmark::State &state) {
-  MatrixTest basicTest;
-  std::string tc_name = "tc1";
-  basicTest.load_data(tc_name);
-
-  matrix::Matrix res = basicTest.construct_matrix(basicTest.c.M, basicTest.c.N);
+static void BM_BasicSquareKernel(benchmark::State &state) {
+  void *devA, *devB, *devC;
+  int32_t m = state.range(0);
+  int32_t p = state.range(0);
+  int32_t n = state.range(0);
+  cudaMalloc(&devA, m * p * sizeof(float));
+  cudaMalloc(&devB, p * n * sizeof(float));
+  cudaMalloc(&devC, m * n * sizeof(float));
   for (auto _ : state)
-    matrix::multiply(res.data, basicTest.a.data, basicTest.b.data, basicTest.a.M, basicTest.a.N, basicTest.b.N);
-
-  basicTest.free_data();
-  free(res.data);
+    matrix::run_multiply_kernel(static_cast<float *>(devC), static_cast<float *>(devA), static_cast<float *>(devB), m, p, n);
+  cudaFree(devA);
+  cudaFree(devB);
+  cudaFree(devC);
 }
 
-BENCHMARK(BM_WithBinaryFiles);
+BENCHMARK(BM_BasicSquareKernel)->RangeMultiplier(2)->Range(2, 1 << 10);
 
 BENCHMARK_MAIN();
