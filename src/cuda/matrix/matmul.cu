a265afdf086478f735971148902ab4f23739974d
Milosz Malczak <milosz.malczak@gmail.com>
2023-01-18 09:02:17 +0100

    Use shared memory

--- src/cuda/matrix/matmul.cu
+++ src/cuda/matrix/matmul.cu
@@ -6,14 +6,34 @@
 namespace matrix {
 
 __global__ static void multiply_kernel(float *C, float *A, float *B, int32_t m, int32_t p, int32_t n) {
-  for (int32_t i = blockIdx.y * blockDim.y + threadIdx.y; i < m; i += gridDim.y * blockDim.y) {
-    for (int32_t j = blockIdx.x * blockDim.x + threadIdx.x; j < n; j += gridDim.x * blockDim.x) {
-      int32_t k;
+  extern __shared__ float sh_mem[];
+  float *As = &sh_mem[0];
+  float *Bs = &sh_mem[blockDim.x*blockDim.y];
+  int m1 = ((m+blockDim.y-1)/blockDim.y) * blockDim.y;
+  int n1 = ((n+blockDim.x-1)/blockDim.x) * blockDim.x;
+  int pp = (p+blockDim.x-1)/blockDim.x;
+
+  int idx = threadIdx.x + blockDim.x*threadIdx.y;
+  for (int32_t i = blockIdx.y * blockDim.y + threadIdx.y; i < m1; i += gridDim.y * blockDim.y) {
+    for (int32_t j = blockIdx.x * blockDim.x + threadIdx.x; j < n1; j += gridDim.x * blockDim.x) {
+
       float s = 0;
-      for (k = 0; k < p; k++) {
-        s += A[i * p + k] * B[k * n + j];
+      for(int32_t t=0; t<pp;t++) {
+        int kx = threadIdx.x + t*blockDim.x;
+        int ky = threadIdx.y + t*blockDim.y;
+        As[idx] = (i<m && kx<p)?A[i*p+kx]:0;
+        Bs[idx] = (j<n && ky<p)?B[j+ky*n]:0;
+        __syncthreads();
+
+        for(int k =0; k<blockDim.x; k++) {
+          s += As[k+threadIdx.y*blockDim.x] * Bs[k*blockDim.x+threadIdx.x];
+        }
+        __syncthreads();
+
       }
-      C[i * n + j] = s;
+      if(i<m && j<n){
+        C[i * n + j] = s;
+      }
     }
   }
 }
@@ -24,7 +44,9 @@ void run_multiply_kernel(float *devC, float *devA, float *devB, int32_t m, int32
   int32_t gridSizeY = 32;
   dim3 dimBlock(K, K);
   dim3 dimGrid(gridSizeX, gridSizeY);
-  matrix::multiply_kernel<<<dimGrid, dimBlock>>>(devC, devA, devB, m, p, n);
+  size_t shSize = 2 * K * K* sizeof(float);
+
+  matrix::multiply_kernel<<<dimGrid, dimBlock, shSize>>>(devC, devA, devB, m, p, n);
 }
 
 void multiply(float *C, float *A, float *B, int32_t m, int32_t p, int32_t n) {
